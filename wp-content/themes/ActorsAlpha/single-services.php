850371f9df0d0dcc9f30027b2bbe4110a97be487
Zac <ztaylor555@gmail.com>
2018-09-25 14:13:08 -0400

    minor update

--- wp-content/themes/ActorsAlpha/single-services.php
+++ wp-content/themes/ActorsAlpha/single-services.php
@@ -5,7 +5,9 @@
     <?php get_template_part('template-parts/hero-blog'); ?>
     
     <section class="page-intro__text-box page-intro__text-box--blog e-fade-in-bottom">
+    
         <?php the_content(); ?>
+
     </section>
     
     <?php get_template_part('template-parts/promos/quote'); ?>
